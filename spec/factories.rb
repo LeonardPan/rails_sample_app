409a9d96dbe4e8112d50c9e6bb11336733ff3183
LeonardPan <panyong.zju@gmail.com>
2013-09-23 21:00:56 +0800

    before the design of signup form

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
 	factory :user do
-		sequence(:name) { |n| "Person #{n}" }
-		sequence(:email) { |n| "person #{n}@example.com"}
+		name "Michael Hartl"
+		email "michael@example.com"
 		password "foobar"
 		password confirmation "foobar"
 	end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -2,12 +2,21 @@ require 'spec_helper'
 
 describe "UserPages" do
 	subject { page }
-  describe "GET /user_pages" do
+  describe "signup page" do
     before { visit signup_path }
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       it { should have_selector('h1', text: 'Sign up') }
       it { should have_selector('title', text: full_title('Sign up'))}
   end
+
+  describe "profile page" do
+	# Code to make a user variable
+	let(:user) { FactoryGirl.create(:user) }
+	before { visit user_path(user) }
+	
+		it { should have_selector('h1', text: user.name) }
+		it { should have_selector('title', text: user.name) }
+	end
 
   describe "index" do
   	let(:user) { FactoryGirl.create(:user) }
