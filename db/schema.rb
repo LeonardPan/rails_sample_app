630de82d6617e39eadc0ccbeb4c8f532ef7ca870
LeonardPan <panyong.zju@gmail.com>
2013-09-24 17:20:23 +0800

    Finish user signup

--- db/schema.rb
+++ db/schema.rb
@@ -9,11 +9,11 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended that you check this file into your version control system.
+# It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130922120542) do
+ActiveRecord::Schema.define(:version => 20130922120542) do
 
-  create_table "users", force: true do |t|
+  create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at"
@@ -21,6 +21,6 @@ ActiveRecord::Schema.define(version: 20130922120542) do
     t.string   "password_digest"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,40 +1,50 @@
 require 'spec_helper'
 
 describe "UserPages" do
-	subject { page }
-  describe "signup page" do
-    before { visit signup_path }
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      it { should have_selector('h1', text: 'Sign up') }
-      it { should have_selector('title', text: full_title('Sign up'))}
-  end
 
-  describe "profile page" do
-	# Code to make a user variable
-	let(:user) { FactoryGirl.create(:user) }
-	before { visit user_path(user) }
-	
-		it { should have_selector('h1', text: user.name) }
-		it { should have_selector('title', text: user.name) }
+	subject { page }
+
+	describe "index" do
+		let(:user) { FactoryGirl.create(:user) }
+		before(:each) do
+			sign_in user
+			visit users_path
+		end
+		it { should have_selector('title', text: 'All users') }
+		it { should have_selector('h1', text:'All users') }
+
+		describe "pagination" do
+			it { should have_selector('div.pagination') }
+
+			it "should list each user" do
+				User.pagination(page: 1).each do |user|
+					page.should have_selector('li', text: user.name )
+				end
+			end
+		end
 	end
 
-  describe "index" do
-  	let(:user) { FactoryGirl.create(:user) }
-  	before(:each) do
-  		sign_in user
-  		visit users_path
-  	end
-  	it { should have_selector('title', text: 'All users') }
-  	it { should have_selector('h1', text:'All users') }
+	describe "signup page" do
+		before { visit signup_path }
+		let(:submit) { "Create my account" }
 
-  	describe "pagination" do
-  		it { should have_selector('div.pagination') }
+		describe "with invalid information" do
+			it "should not create a user" do
+				expect { click_button submit }.not_to change(User, :count)
+			end
+		end
 
-  		it "should list each user" do
-  			User.pagination(page: 1).each do |user|
-  				page.should have_selector('li', text: user.name )
-  			end
-  		end
-  	end
-  end
+		describe "with valid information" do
+			before do
+				fill_in "Name",		with: "Example User"
+				fill_in "Email",	with: "user@example.com"
+				fill_in "Password",	with: "foobar"
+				fill_in "Confirmation",	with: "foobar"
+			end
+
+			it "should create a user" do
+				expect { click_button submit }.to change(User, :count).by(1)
+			end
+		end
+	end
 end
