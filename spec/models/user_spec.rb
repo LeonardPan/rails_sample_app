840a17414ca72496b5ad00659d6499e7803af9b3
LeonardPan <panyong.zju@gmail.com>
2013-10-23 17:51:50 +0800

    page 511

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -28,6 +28,8 @@ describe User do
   it { should respond_to(:followed_users) }
   it { should respond_to(:following?) }
   it { should respond_to(:follow!) }
+  it { should respond_to(:reverse_relationships) }
+  it { should respond_to(:followers) }
 
   it { should be_valid }
 
@@ -152,6 +154,11 @@ describe User do
 
     it { should be_following(other_user) }
     its(:followed_users) { should include(other_user) }
+
+    describe "follwing user" do
+      subject { other_user }
+      its(:followers) { should include(@user) }
+    end
 
     describe "and unfollowing" do
       before { @user.unfollow!(other_user) }
