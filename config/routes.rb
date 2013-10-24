5919525e0f666a0d3e34ffd14aa2af524bb6b916
LeonardPan <panyong.zju@gmail.com>
2013-10-24 17:28:39 +0800

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ SampleApp::Application.routes.draw do
   
   resources :session, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   #get "users/new"
   #get "static_pages/home"
   #get '/home', to: 'static_pages#home'
